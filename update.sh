11b98c4bea141d42015424fb2182e798a4429bc0
KnoAll <7632685+KnoAll@users.noreply.github.com>
2018-11-16 11:54:49 -0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+# bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/master/update.sh)
+
 if [[ `whoami` == "root" ]]; then
     echo "You ran me as root! Do not run me as root!"
     exit 1
 fi
-
-# bash <(curl -s http://www.kevinnoall.com/x/interactive.sh)
 
 function upgrade-git () {
 echo "Upgrading Git"
@@ -18,7 +18,39 @@ function backup-db () {
 mysqldump --user=cacti --password=cacti -l --add-drop-table cacti |gzip > mysql.cacti_$(date +\%Y\%m\%d).sql.gz
 }
 
-function cacti-upgrade () {
-echo "Begining Cacti upgrade."
+function upgrade-cacti () {
+echo "Begining Cacti upgrade..."
 cd /var/www/html/cacti
+git -C cacti/ pull --no-edit
+}
+
+function upgrade-plugins () {
+echo "Upgrading plugins..."
+cd /var/www/html/
+git -C cacti/plugins/mactrack/ pull --no-edit
+git -C cacti/plugins/monitor/ pull --no-edit
+git -C cacti/plugins/webseer/ pull --no-edit
+git -C cacti/plugins/gexport/ pull --no-edit
+git -C cacti/plugins/syslog/ pull --no-edit
+git -C cacti/plugins/thold/ pull --no-edit
+git -C cacti/plugins/routerconfigs/ pull --no-edit
+git -C cacti/plugins/flowview/ pull --no-edit
+git -C cacti/plugins/maint/ pull --no-edit
+git -C cacti/plugins/audit/ pull --no-edit
+git -C cacti/plugins/cycle/ pull --no-edit
+git -C cacti/plugins/weathermap/ pull --no-edit
+echo "Installing Weathermap plugin"
+cd cacti/plugins/weathermap/
+bower install --allow-root
+composer update --no-dev
+}
+
+function update-config () {
+echo "Updating cacti config..."
+cp cacti/include/config.php.dist cacti/include/config.php
+echo "$(awk '{sub(/cactiuser/,"cacti")}1' cacti/include/config.php)" > cacti/include/config.php
+}
+
+function upgrade-spine () {
+echo "Upgrading spine..."
 }
