00c335b174fda3ddf342c1bdbaa539e94f61be2a
KnoAll <7632685+KnoAll@users.noreply.github.com>
2018-11-16 14:41:23 -0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 # bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/master/update.sh)
-echo "Welcome!"
-
-if [ -f "~/.cacti-template" ]
-then
-	echo "Found preexisting Cacti Install, proceeding to upgrade..."
-else
-	echo "Cacti is not already insdalled, sorry cannot upgrade. Exiting..."
-    sleep 10
-    exit 1
-fi
 
 if [[ `whoami` == "root" ]]; then
     echo "You ran me as root! Do not run me as root!"
     exit 1
 fi
+
+if [ -f ~/.cacti-template ]
+then
+	echo "Found preexisting Cacti Install, proceeding to upgrade..."
+else
+	echo "Cacti is not already installed, sorry cannot upgrade. Exiting..."
+    sleep 5
+    exit 1
+fi
+
+echo "Welcome to Kevin's Cacti Template upgrade script!"
 
 function upgrade-git () {
 echo "Upgrading Git"
