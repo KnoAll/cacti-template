5b63dd58ecf80d97c5479a29183f1ca66b745938
KnoAll <7632685+KnoAll@users.noreply.github.com>
2018-11-16 16:21:31 -0800

    Update wrapper.sh

--- wrapper.sh
+++ wrapper.sh
@@ -14,15 +14,4 @@ else
   chmod g+x cacti-upgrade.sh
 fi
 
-if [ -f ~/.cacti-template ]
-then
-	echo "Found preexisting cacti-template Install, proceeding to upgrade..."
-	echo ""
-  bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/master/update-git.sh)
-else
-	echo "Cacti is not already installed, sorry cannot upgrade. Exiting..."
-    sleep 5
-    exit 1
-fi
-
-
+./cacti-upgrade.sh
