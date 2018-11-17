70d034851a120d78cb3bd442105d0be346fc89a4
KnoAll <7632685+KnoAll@users.noreply.github.com>
2018-11-16 16:01:17 -0800

    Update and rename cacti-upgrade.sh to kacti-upgrade.sh

--- cacti-upgrade.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/master/update-git.sh)
---
+++ kacti-upgrade.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [[ `whoami` == "root" ]]; then
+    echo "You ran me as root! Do not run me as root!"
+    exit 1
+fi
+
+if [ -f ~/kacti-upgrade.sh ]
+then
+else
+	echo "Downloading kacti-upgrade.sh"
+  wget https://raw.githubusercontent.com/KnoAll/cacti-template/master/kacti-upgrade.sh
+exit 1
+fi
+
+if [ -f ~/.kacti-template ]
+then
+	echo "Found preexisting Kacti-template Install, proceeding to upgrade..."
+	echo ""
+  bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/master/update-git.sh)
+else
+	echo "Cacti is not already installed, sorry cannot upgrade. Exiting..."
+    sleep 5
+    exit 1
+fi
+
+
