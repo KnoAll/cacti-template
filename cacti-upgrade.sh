0b5b94941bc840fc6c690495f81048277585840d
KnoAll <7632685+KnoAll@users.noreply.github.com>
2018-11-16 16:28:43 -0800

    Update cacti-upgrade.sh

--- cacti-upgrade.sh
+++ cacti-upgrade.sh
@@ -2,8 +2,6 @@
 
 if [ -f ~/.cacti-template ]
 then
-	echo "Found preexisting cacti-template Install, proceeding to upgrade..."
-	echo ""
   bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/master/update-git.sh)
 else
 	echo "Cacti is not already installed, sorry cannot upgrade. Exiting..."
