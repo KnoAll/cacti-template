f525ce33daedac3e86fea4654ae9c5fc5d0656aa
KnoAll <7632685+KnoAll@users.noreply.github.com>
2018-11-19 15:29:17 -0800

    Update update-pre-git.sh

--- update-pre-git.sh
+++ update-pre-git.sh
@@ -25,7 +25,7 @@ echo ""
 
 function upgrade-git () {
 echo "Upgrading Git"
-rpm -U http://opensource.wandisco.com/centos/7/git/x86_64/wandisco-git-release-7-2.noarch.rpm \
+sudo rpm -U http://opensource.wandisco.com/centos/7/git/x86_64/wandisco-git-release-7-2.noarch.rpm \
     && yum install -y git	
 echo ""
 }
