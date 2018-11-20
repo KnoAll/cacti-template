3fedf941a148cc82d5d805a619ae7508840505b9
KnoAll <7632685+KnoAll@users.noreply.github.com>
2018-11-19 20:25:38 -0800

    Update update-git.sh

--- update-git.sh
+++ update-git.sh
@@ -23,7 +23,7 @@ function version_gt() {
 test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1";
 }
 
-upgrade_version=1.1.20
+upgrade_version=1.1.40
 if version_gt $cactiver $upgrade_version; then
      echo "Installed cacti version $cactiver is greater than required $upgrade_version! Proceeding to upgrade..."
 else
