0898b9426fc8166ac84ea05da73331016227a5fe
KnoAll <7632685+KnoAll@users.noreply.github.com>
2018-11-20 10:27:22 -0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/master/update-git.sh)
+# bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/master/bootstrap.sh)
 
 if [[ `whoami` == "root" ]]; then
     echo "You ran me as root! Do not run me as root!"
     exit 1
+    elif [[ `whoami` == "cacti" ]]; then
+    echo ""
+    else
+    echo "Uh-oh. You are not logged in as the cacti user. Exiting..."
 fi
 
 if [ -f ~/cacti-upgrade.sh ]
