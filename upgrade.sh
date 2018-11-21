3fbc8c3581975f06f5dfa7b0675fd52fc4b2bf02
KnoAll <7632685+KnoAll@users.noreply.github.com>
2018-11-20 22:39:29 -0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -16,20 +16,19 @@ cactiver=$( cat /var/www/html/cacti/include/cacti_version )
 upgrade_version=1.1.0
 prod_version=1.1.38
 
-function version_gt() { 
-test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1";
-}
+function version_ge() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1"; }
 
-if version_gt $cactiver $upgrade_version; then
-	if version_gt $cactiver $prod_version; then
-		echo "Current installed Cacti v$cactiver is current! No need to upgrade, exiting..."
-		exit 0
-	else
-		echo "Installed cacti v$cactiver is greater than required v$upgrade_version! Proceeding to upgrade..."
-	fi
+if version_ge $cactiver $upgrade_version; then
+        if version_ge $cactiver $prod_version; then
+                echo "Cacti v$cactiver is up to date or newer than production v$prod_version, nothing to do, exiting!"
+                exit 0
+        else
+		bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/master/upgrade.sh)
+                exit
+        fi
 else
-	echo "Current cacti install v$cactiver is not compatible with upgrade version v$upgrade_version, exiting..."
-	exit 1
+                echo "Cacti v$cactiver is less than upgrade version v$upgrade_version cannot install, exiting..."
+                exit
 fi
 
 #if [ -f ~/.cacti-template ]
