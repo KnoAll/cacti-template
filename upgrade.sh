ee3f1931aaee0b2ebb389fc447036609ce2654bc
KnoAll <7632685+KnoAll@users.noreply.github.com>
2018-12-04 10:41:13 -0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -48,6 +48,7 @@ if version_ge $smokever $smokeping_version; then
 		echo -e -n "\033[0m"
 		read -n 1 -p "y/n: " smokeup
         	if [ "$smokeup" = "y" ]; then
+			echo "Do the Smokeping"
 #			bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/dev/smokeping-upgrade.sh)
 		fi
         fi
