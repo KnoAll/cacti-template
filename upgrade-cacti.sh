15029f033f212b57dcd4849ffbccfe3bd46755ae
KnoAll <7632685+KnoAll@users.noreply.github.com>
2018-12-18 15:04:55 -0800

    Update upgrade-cacti.sh

--- upgrade-cacti.sh
+++ upgrade-cacti.sh
@@ -48,10 +48,12 @@ if [ $? -ne 0 ];then
 fi
 if version_ge $smokever $smokeping_version; then
         if version_ge $smokever $smokeping_prod_version; then
+		echo ""
                 echo -e "\033[32m Smokeping v$smokever is up to date with production v$smokeping_prod_version, nothing to do, exiting!"
 		echo -e -n "\033[0m"
 		smokeping_onoff
         else
+		echo ""
 		echo -e "\033[32m Installed Smokeping v$smokever is compatible with required v$smokeping_version! Do you wish to upgrade?"
 		echo -e -n "\033[0m"
 		read -n 1 -p "y/n: " smokeup
