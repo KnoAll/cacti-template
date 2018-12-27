110752be64bae19088e7f97c69af2f7bf1e4cf94
KnoAll <7632685+KnoAll@users.noreply.github.com>
2018-12-27 13:59:00 -0800

    set to prod version for push to master

--- upgrade-cacti.sh
+++ upgrade-cacti.sh
@@ -18,7 +18,7 @@ fi
 upgrade_version=1.1.6
 # get ready for dynamic update
 #prod_version=( curl -s https://raw.githubusercontent.com/Cacti/cacti/master/include/cacti_version )
-prod_version=1.2.0-beta4
+prod_version=1.1.38
 dev_version=1.2.0-beta4
 symlink_cactidir=1.1.28
 cactiver=$( cat /var/www/html/cacti/include/cacti_version )
