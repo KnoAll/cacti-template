71f418edf665f97b23b33c8201c3a10dd941f047
KnoAll <7632685+KnoAll@users.noreply.github.com>
2018-12-28 15:38:32 -0800

    Update upgrade-plugins.sh

--- upgrade-plugins.sh
+++ upgrade-plugins.sh
@@ -12,3 +12,11 @@ do
   [ $(echo $?) -eq 0 ] && echo "Updating ${dir%*/}..." && git pull
   cd ..
 done
+
+function update-syslog-config () {
+echo -e "\033[32m Updating syslog plugin config..."
+echo -e -n "\033[0m"
+cd /var/www/html/
+cp cacti/plugins/syslog/config.php.dist cacti/plugins/syslog/config.php
+sed -i 's/cactiuser/cacti/g' cacti/plugins/syslog/config.php
+}
