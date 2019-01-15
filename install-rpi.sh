be7360b13caf1167766261a9a9783fdab96af0b5
KnoAll <7632685+KnoAll@users.noreply.github.com>
2019-01-15 09:38:24 -0800

    added user check

--- install-rpi.sh
+++ install-rpi.sh
@@ -2,6 +2,18 @@
 
 # bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/rpi-template/install-rpi.sh)
 
+if [[ `whoami` == "root" ]]; then
+    echo -e "\033[31m You ran me as root! Do not run me as root!"
+    echo -e -n "\033[0m"
+    exit 1
+    elif [[ `whoami` == "pi" ]]; then
+    	echo ""
+    else
+    echo -e "\033[31m Uh-oh. You are not logged in as the default pi user. Exiting..."
+    echo -e -n "\033[0m"
+    exit 1
+fi
+
 echo -e "\033[32m Updating Raspian"
 echo -e -n "\033[0m"
 sudo apt update; sudo apt -y upgrade
