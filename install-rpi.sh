82d1da5d87eeb0438ccee7ba89e794d70163b2a1
KnoAll <7632685+KnoAll@users.noreply.github.com>
2019-01-16 15:13:14 -0800

    Update install-rpi.sh

--- install-rpi.sh
+++ install-rpi.sh
@@ -148,15 +148,23 @@ fi
 echo -e "\033[32m Updating Apache permissions"
 echo -e -n "\033[0m"
 sudo chown -R www-data:www-data /var/www/html/
-sudo chmod -R g+w /var/www/html/
-
-sudo find /var/www/html -type d -exec chmod g+rwx {} +
-sudo find /var/www/html -type f -exec chmod g+rw {} +
-sudo find /var/www/html -type d -exec chmod u+rwx {} +
-sudo find /var/www/html -type f -exec chmod u+rw {} +
-sudo find /var/www/html -type d -exec chmod g+s {} +
-sudo find /var/www/html -type f -exec chmod g+s {} +
-
+if [ $? -ne 0 ];then
+	echo -e "\033[31m Something went updating permissions, exiting..."
+	exit 1
+else
+	sudo chmod -R g+w /var/www/html/
+		if [ $? -ne 0 ];then
+			echo -e "\033[31m Something went updating permissions, exiting..."
+			exit 1
+		else
+			sudo find /var/www/html -type d -exec chmod g+rwx {} +
+			sudo find /var/www/html -type f -exec chmod g+rw {} +
+			sudo find /var/www/html -type d -exec chmod u+rwx {} +
+			sudo find /var/www/html -type f -exec chmod u+rw {} +
+			sudo find /var/www/html -type d -exec chmod g+s {} +
+			sudo find /var/www/html -type f -exec chmod g+s {} +
+		fi	
+fi
 
 function update-php () {
 if version_ge $prod_version 1.2.0; then
