80c3c73153f9147e422e9844b5be94f210e6ecfa
KnoAll <7632685+KnoAll@users.noreply.github.com>
2019-01-16 11:30:15 -0800

    dynamic dev version

--- upgrade-cacti.sh
+++ upgrade-cacti.sh
@@ -19,7 +19,7 @@ upgrade_version=1.1.6
 # get ready for dynamic update
 #prod_version=( curl -s https://raw.githubusercontent.com/Cacti/cacti/master/include/cacti_version )
 prod_version=1.2.0
-dev_version=1.2.0-beta4
+dev_version=( curl -s https://raw.githubusercontent.com/Cacti/cacti/develop/include/cacti_version )
 symlink_cactidir=1.1.28
 cactiver=$( cat /var/www/html/cacti/include/cacti_version )
 if [ $? -ne 0 ];then
