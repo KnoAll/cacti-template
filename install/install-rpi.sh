a778a00a0a26e94e1a7b0a9d72e151590b5a2d87
KnoAll <7632685+KnoAll@users.noreply.github.com>
2019-01-21 09:35:07 -0800

    change prod_version for new cacti v1.2.1 release

--- install/install-rpi.sh
+++ install/install-rpi.sh
@@ -21,7 +21,7 @@ fi
 # get the Cacti version
 # get ready for dynamic update
 #prod_version=( curl -s https://raw.githubusercontent.com/Cacti/cacti/master/include/cacti_version )
-prod_version=1.2.0
+prod_version=1.2.1
 cactiver=$( cat /var/www/html/cacti/include/cacti_version )
 if [ $? -ne 1 ];then
 	echo -e "\033[31m Cacti is already installed, cannot proceed..."
