bcb6306270496d2ead862cea082461941a1cc493
KnoAll <7632685+KnoAll@users.noreply.github.com>
2019-01-23 15:31:11 -0800

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -30,7 +30,7 @@ fi
 # get ready for dynamic update
 #prod_version=( curl -s https://raw.githubusercontent.com/Cacti/cacti/master/include/cacti_version )
 prod_version=1.2.0
-cactiver=$( cat /var/www/html/cacti/include/cacti_version )
+test -f /var/www/html/cacti/include/cacti_version
 if [ $? -ne 1 ];then
 	echo -e "\033[31m Cacti is already installed, cannot proceed..."
 	echo -e -n "\033[0m"
