55da5d3095718274c9bd63737a7fc6d3fd1a3b5f
KnoAll <7632685+KnoAll@users.noreply.github.com>
2019-01-25 14:21:52 -0800

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -225,32 +225,39 @@ fi
 
 echo -e "\033[32m Updating mysql for Cacti v1.2.x"
 echo -e -n "\033[0m"
-if [[ $os_dist == "centos" ]]; then
-		mycnf_path=/etc/my.cnf
+if [[ $os_dist == "raspbian" ]]; then
+	mycnf_path=/etc/mysql/my.cnf
+	dbserver=mysql
+elif [[ $os_dist == "centos" ]]; then
+	mycnf_path=/etc/my.cnf
+	dbserver=mariadb
+fi
+	grep -q -w "mysqld" $mycnf_path
+	if [ $? -ne 0 ];then
+		#Fugly but works for now...
+		sudo sed  -i '$ a [mysqld]' $mycnf_path
+		sudo sed  -i '$ a max_allowed_packet=16M' $mycnf_path
+		sudo sed  -i '$ a innodb_additional_mem_pool_size=80M' $mycnf_path 
+		sudo sed  -i '$ a innodb_flush_log_at_timeout=3' $mycnf_path 
+		sudo sed  -i '$ a innodb_read_io_threads=32' $mycnf_path
+		sudo sed  -i '$ a innodb_write_io_threads=16' $mycnf_path 
+		sudo sed  -i '$ a max_heap_table_size=30M' $mycnf_path 
+		sudo sed  -i '$ a tmp_table_size=30M' $mycnf_path 
+		sudo sed  -i '$ a join_buffer_size=58M' $mycnf_path 
+		sudo sed  -i '$ a innodb_buffer_pool_size=450M' $mycnf_path 
+		sudo sed  -i '$ a character-set-server=utf8mb4' $mycnf_path 
+		sudo sed  -i '$ a collation-server=utf8mb4_unicode_ci' $mycnf_path 
+		sudo sed  -i '$ a max_allowed_packet=16M' $mycnf_path 
+		sudo systemctl restart $dbserver.service
+			if [ $? -ne 0 ];then
+			echo -e "\033[31m Something went wrong restarting mysql, exiting..."
+			echo -e -n "\033[0m"
+			exit 1
+			fi
 	else
-		mycnf_path=/etc/mysql/my.cnf
+		echo ""
 	fi
-		grep -q -w "mysqld" $mycnf_path
-		if [ $? -ne 0 ];then
-			#Fugly but works for now...
-			sudo sed  -i '$ a [mysqld]' $mycnf_path
-			sudo sed  -i '$ a max_allowed_packet=16M' $mycnf_path
-			sudo sed  -i '$ a innodb_additional_mem_pool_size=80M' $mycnf_path 
-			sudo sed  -i '$ a innodb_flush_log_at_timeout=3' $mycnf_path 
-			sudo sed  -i '$ a innodb_read_io_threads=32' $mycnf_path
-			sudo sed  -i '$ a innodb_write_io_threads=16' $mycnf_path 
-			sudo sed  -i '$ a max_heap_table_size=30M' $mycnf_path 
-			sudo sed  -i '$ a tmp_table_size=30M' $mycnf_path 
-			sudo sed  -i '$ a join_buffer_size=58M' $mycnf_path 
-			sudo sed  -i '$ a innodb_buffer_pool_size=450M' $mycnf_path 
-			sudo sed  -i '$ a character-set-server=utf8mb4' $mycnf_path 
-			sudo sed  -i '$ a collation-server=utf8mb4_unicode_ci' $mycnf_path 
-			sudo sed  -i '$ a max_allowed_packet=16M' $mycnf_path 
-		else
-			echo "put in other mysqld stuff here"
-		fi
-	sudo systemctl restart mysqld.service
-	fi
+
 fi
 
 echo -e "\033[32m Setting up Cacti"
