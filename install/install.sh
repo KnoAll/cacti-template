77efb61c817115b56586840687dd283544e87126
KnoAll <7632685+KnoAll@users.noreply.github.com>
2019-01-25 16:03:45 -0800

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -276,26 +276,52 @@ fi
 
 echo -e "\033[32m Updating Apache permissions"
 echo -e -n "\033[0m"
-sudo chown -R www-data:www-data /var/www/html/
-if [ $? -ne 0 ];then
-	echo -e "\033[31m Something went updating permissions, exiting..."
-	echo -e -n "\033[0m"
-	exit 1
-else
-	sudo chmod -R g+w /var/www/html/
-		if [ $? -ne 0 ];then
-			echo -e "\033[31m Something went updating permissions, exiting..."
-			echo -e -n "\033[0m"
-			exit 1
-		else
-			sudo find /var/www/html -type d -exec chmod g+rwx {} +
-			sudo find /var/www/html -type f -exec chmod g+rw {} +
-			sudo find /var/www/html -type d -exec chmod u+rwx {} +
-			sudo find /var/www/html -type f -exec chmod u+rw {} +
-			sudo find /var/www/html -type d -exec chmod g+s {} +
-			#sudo find /var/www/html -type f -exec chmod g+s {} +
-		fi	
+if [[ $os_dist == "raspbian" ]]; then
+	sudo chown -R www-data:www-data /var/www/html/
+	if [ $? -ne 0 ];then
+		echo -e "\033[31m Something went updating permissions, exiting..."
+		echo -e -n "\033[0m"
+		exit 1
+	else
+		sudo chmod -R g+w /var/www/html/
+			if [ $? -ne 0 ];then
+				echo -e "\033[31m Something went updating permissions, exiting..."
+				echo -e -n "\033[0m"
+				exit 1
+			else
+				sudo find /var/www/html -type d -exec chmod g+rwx {} +
+				sudo find /var/www/html -type f -exec chmod g+rw {} +
+				sudo find /var/www/html -type d -exec chmod u+rwx {} +
+				sudo find /var/www/html -type f -exec chmod u+rw {} +
+				sudo find /var/www/html -type d -exec chmod g+s {} +
+				#sudo find /var/www/html -type f -exec chmod g+s {} +
+			fi	
+	fi
+elif [[ $os_dist == "centos" ]]; then
+	sudo chown -R cacti:apache /var/www/html/
+	if [ $? -ne 0 ];then
+		echo -e "\033[31m Something went updating permissions, exiting..."
+		echo -e -n "\033[0m"
+		exit 1
+	else
+		sudo chmod -R g+w /var/www/html/
+			if [ $? -ne 0 ];then
+				echo -e "\033[31m Something went updating permissions, exiting..."
+				echo -e -n "\033[0m"
+				exit 1
+			else
+				sudo find /var/www/html -type d -exec chmod g+rwx {} +
+				sudo find /var/www/html -type f -exec chmod g+rw {} +
+				sudo find /var/www/html -type d -exec chmod u+rwx {} +
+				sudo find /var/www/html -type f -exec chmod u+rw {} +
+				sudo find /var/www/html -type d -exec chmod g+s {} +
+				#sudo find /var/www/html -type f -exec chmod g+s {} +
+			fi	
+	fi
 fi
+grep -q -w "mysqld" $mycnf_path
+if [ $? -ne 0 ];then
+
 function version_ge() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1"; }
 function update-php () {
 if version_ge $prod_version 1.2.0; then
