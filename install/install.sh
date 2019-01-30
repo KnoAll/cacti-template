44f4bb7bd56cec0628999735b4903e0313019132
KnoAll <7632685+KnoAll@users.noreply.github.com>
2019-01-30 08:22:06 -0800

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -111,7 +111,7 @@ if [[ $os_dist == "raspbian" ]]; then
 	fi
 elif [[ $os_dist == "centos" ]]; then
 	curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
-	sed -i 's/enforcing/disabled/g' /etc/selinux/config
+	sudo sed -i 's/enforcing/disabled/g' /etc/selinux/config
 	sudo yum install -y -q httpd php php-mysql MariaDB-server rrdtool net-snmp net-snmp-utils autoconf automake libtool dos2unix help2man openssl-devel MariaDB-devel net-snmp-devel nano wget git php-gd php-mbstring php-snmp php-ldap php-posix
 	if [ $? -ne 0 ];then
 		echo -e "\033[31m Something went wrong installing prerequisites, exiting..."
