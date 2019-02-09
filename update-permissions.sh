ec1a822d0df30109386df5ae2488355371513c71
KnoAll <7632685+KnoAll@users.noreply.github.com>
2019-02-09 14:16:12 -0800

    Update update-permissions.sh

--- update-permissions.sh
+++ update-permissions.sh
@@ -3,7 +3,10 @@
 echo -e "\033[32m Updating Apache permissions"
 echo -e -n "\033[0m"
 if [[ $os_dist == "raspbian" ]]; then
-	sudo chown -R www-data:www-data /var/www/html/
+	sudo chgrp -R www-data /var/www/html/cacti/log
+	sudo chgrp -R www-data /var/www/html/cacti/resource
+	sudo chgrp -R www-data /var/www/html/cacti/cache
+	sudo chgrp -R www-data /var/www/html/cacti/scripts
 	if [ $? -ne 0 ];then
 		echo -e "\033[31m Something went updating permissions, exiting..."
 		echo -e -n "\033[0m"
@@ -20,11 +23,13 @@ if [[ $os_dist == "raspbian" ]]; then
 				sudo find /var/www/html -type d -exec chmod u+rwx {} +
 				sudo find /var/www/html -type f -exec chmod u+rw {} +
 				sudo find /var/www/html -type d -exec chmod g+s {} +
-				#sudo find /var/www/html -type f -exec chmod g+s {} +
 			fi	
 	fi
 elif [[ $os_dist == "centos" ]]; then
-	sudo chown -R cacti:apache /var/www/html/
+	sudo chgrp -R apache /var/www/html/cacti/log
+	sudo chgrp -R apache /var/www/html/cacti/resource
+	sudo chgrp -R apache /var/www/html/cacti/cache
+	sudo chgrp -R apache /var/www/html/cacti/scripts
 	if [ $? -ne 0 ];then
 		echo -e "\033[31m Something went updating permissions, exiting..."
 		echo -e -n "\033[0m"
@@ -41,7 +46,6 @@ elif [[ $os_dist == "centos" ]]; then
 				sudo find /var/www/html -type d -exec chmod u+rwx {} +
 				sudo find /var/www/html -type f -exec chmod u+rw {} +
 				sudo find /var/www/html -type d -exec chmod g+s {} +
-				#sudo find /var/www/html -type f -exec chmod g+s {} +
 			fi	
 	fi
 fi
