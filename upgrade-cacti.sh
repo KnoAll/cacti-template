da341bbf4002a4c18a557db3d7ead502540ca1f6
KnoAll <7632685+KnoAll@users.noreply.github.com>
2019-02-10 15:46:47 -0800

    Update upgrade-cacti.sh

--- upgrade-cacti.sh
+++ upgrade-cacti.sh
@@ -322,29 +322,7 @@ fi
 }
 
 function update-permissions () {
-echo -e "\033[32m Fixing file permissions..."
-echo -e -n "\033[0m"
-if [[ $pkg_mgr == "yum" ]]; then
-	perm_grp=apache
-else
-	perm_grp=www-data
-fi
-groups | grep -q '\$permgrp\b'
-if [ $? -ne 0 ];then
-sudo usermod -a -G $perm_grp cacti
-fi
-sudo chgrp -R $perm_grp /var/www/html/cacti/log
-sudo chgrp -R $perm_grp /var/www/html/cacti/resource
-sudo chgrp -R $perm_grp /var/www/html/cacti/cache
-sudo chgrp -R $perm_grp /var/www/html/cacti/scripts
-sudo chown -R cacti /var/www/html
-sudo find /var/www/html -type d -exec chmod g+rwx {} +
-sudo find /var/www/html -type f -exec chmod g+rw {} +
-sudo find /var/www/html -type d -exec chmod u+rwx {} +
-sudo find /var/www/html -type f -exec chmod u+rw {} +
-sudo find /var/www/html -type d -exec chmod g+s {} +
-touch /var/www/html/cacti/log/cacti.log
-chmod g+w /var/www/html/cacti/log/cacti.log
+	bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/$branch/upgrade-permissions.sh)
 }
 
 function upgrade-spine () {
