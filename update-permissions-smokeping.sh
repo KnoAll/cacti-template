e83073c549e5552aacada61b7fed3f24133b99fd
KnoAll <7632685+KnoAll@users.noreply.github.com>
2019-04-19 15:26:31 -0700

    Update update-permissions-smokeping.sh

--- update-permissions-smokeping.sh
+++ update-permissions-smokeping.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/dev/upgrade-permissions-smokeping.sh)
+
 if which yum >/dev/null; then
 	pkg_mgr=yum
 elif which apt >/dev/null; then
