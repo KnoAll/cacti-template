e14db218ce8eafa9279a95ec4e0972ea7645fbae
KnoAll <7632685+KnoAll@users.noreply.github.com>
2019-04-24 15:41:56 -0700

    Update upgrade-plugins.sh

--- upgrade-plugins.sh
+++ upgrade-plugins.sh
@@ -2,14 +2,19 @@
 
 # bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/dev/upgrade-plugins.sh)
 
-if [[ $1 == "dev" ]]; then
-	branch=dev
-elif [[ $1 == "develop" ]]; then
-	branch=develop
-else
-	branch=master
-	counter=$( curl -s http://www.kevinnoall.com/cgi-bin/counter/unicounter.pl?name=upgrade-plugins&write=0 )
-fi
+case $1 in
+	dev)
+		branch=dev
+	;;
+	develop)
+		branch=develop
+	;;
+	*)
+		branch=master
+		counter=$( curl -s http://www.kevinnoall.com/cgi-bin/counter/unicounter.pl?name=upgrade-plugins&write=0 )
+	;;
+esac
+
 
 echo ""
 echo ""
