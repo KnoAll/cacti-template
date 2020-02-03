876d914c7b9c5ab4834b66b2f204cc8927575c77
KnoAll <7632685+KnoAll@users.noreply.github.com>
2020-02-03 15:03:03 -0800

    Update restore-cacti.sh

--- restore-cacti.sh
+++ restore-cacti.sh
@@ -2,33 +2,61 @@
 
 #bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/dev/restore-cacti.sh)
 
+printinfo() {
+  printf "::: ${green}%s${reset}\n" "$@"
+}
+
+printwarn() {
+ printf "${tan}*** WARNING: %s${reset}\n" "$@"
+}
+
+printerror() {
+ printf "${red}*** ERROR: %s${reset}\n" "$@"
+}
+
 case $(whoami) in
         root)
-		echo -e "\033[31m You ran me as root! Do not run me as root!"
-		echo -e -n "\033[0m"
+		printerror "You ran me as root! Do not run me as root!"
 		exit 1
 		;;
         pi)
-		echo -e "\033[31m You ran me as pi user! Do not run me as pi!"
-		echo -e -n "\033[0m"
+		printerror "You ran me as pi user! Do not run me as pi!"
 		exit 1
                 ;;
         cacti)       
-                echo -e "\033[32m Grabbing Cacti db and data and packaging..."
-                echo -e -n "\033[0m"
-                cactiver=$( cat /var/www/html/cacti/include/cacti_version )
-                mkdir cacti_$cactiver
-                mysqldump --user=cacti --password=cacti -l --add-drop-table cacti |gzip > ~/cacti_$cactiver/mysql.cacti_$(date +\%Y\%m\%d).sql.gz
-                cp -R /var/www/html/cacti/rra ~/cacti_$cactiver/rra
-		touch ~/cacti_$cactiver/.cacti-backup
-                tar -pczf ~/backup_cacti-$cactiver_$(date +\%Y\%m\%d).tar.gz -C ~/ cacti_$cactiver
-		rm -rf cacti_$cactiver
-                ;;
+		;;
         *)
-		echo -e "\033[31m Uh-oh. You are not logged in as the cacti user. Exiting..."
-		echo -e -n "\033[0m"
+		printerror "Uh-oh. You are not logged in as the cacti user. Exiting..."
 		exit 1
                 ;;
 esac
 
-exit
+# get file from param - list files for selection?
+# unzip file and check for .cacti-backup
+# drop/restore mysql cacti db
+# dump exiting rra and move backup rra
+# check for proper file permissions
+
+check-cacti() {
+# check existing cacti installation
+	test -f /var/www/html/cacti/include/cacti_version
+	if [ $? -ne 1 ];then
+		printinfo "Valid Cacti install found..."
+		exit 0
+	else
+		printerror "Cacti is not already installed, cannot proceed."
+		exit 1
+	fi
+# backup existing cacti data?
+	read -p "Do you want to continue to backup existing Cacti install before restoring over top? [y/N] " yn
+	case "$yn" in
+		y | Y | yes | YES| Yes ) printinfo "Ok, let's go!"
+		counter=$( curl -s http://www.kevinnoall.com/cgi-bin/counter/unicounter.pl?name=backup-data&write=0 )
+		bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/master/backup-cacti.sh) $1;;
+		* ) exit;;
+	esac
+}
+
+
+
+exit 0
