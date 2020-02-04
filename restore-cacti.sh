d382544ec3147f237b660e681fa3b51ddb3a1007
KnoAll <7632685+KnoAll@users.noreply.github.com>
2020-02-04 09:55:35 -0800

    Update restore-cacti.sh

--- restore-cacti.sh
+++ restore-cacti.sh
@@ -2,7 +2,7 @@
 
 #bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/dev/restore-cacti.sh)
 
-green=$(tput setaf 76)
+green=$(tput setaf 125)
 red=$(tput setaf 1)
 tan=$(tput setaf 3)
 reset=$(tput sgr0)
