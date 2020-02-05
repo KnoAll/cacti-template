6c2a67a4f436fe816eb25473d71798fd150e006b
KnoAll <7632685+KnoAll@users.noreply.github.com>
2020-02-05 09:45:50 -0800

    Update cacti-upgrade.sh

--- cacti-upgrade.sh
+++ cacti-upgrade.sh
@@ -15,6 +15,39 @@ case $(whoami) in
 	;;
 esac
 
+green=$(tput setaf 2)
+red=$(tput setaf 1)
+tan=$(tput setaf 3)
+reset=$(tput sgr0)
+
+welcomeMessage() {
+  echo -n "${tan}"
+  cat << "EOF"
+
+        CCCCCCCCCCCCC                                              tttt            iiii
+     CCC::::::::::::C                                           ttt:::t           i::::i
+   CC:::::::::::::::C                                           t:::::t            iiii
+  C:::::CCCCCCCC::::C                                           t:::::t
+C:::::C       CCCCCC  aaaaaaaaaaaaa      ccccccccccccccccttttttt:::::ttttttt    iiiiiii
+C:::::C                a::::::::::::a   cc:::::::::::::::ct:::::::::::::::::t    i:::::i
+C:::::C                aaaaaaaaa:::::a c:::::::::::::::::ct:::::::::::::::::t     i::::i
+C:::::C                         a::::ac:::::::cccccc:::::ctttttt:::::::tttttt     i::::i
+C:::::C                  aaaaaaa:::::ac::::::c     ccccccc      t:::::t           i::::i
+C:::::C                aa::::::::::::ac:::::c                   t:::::t           i::::i
+C:::::C               a::::aaaa::::::ac:::::c                   t:::::t           i::::i
+C:::::C       CCCCCCa::::a    a:::::ac::::::c     ccccccc      t:::::t    tttttt i::::i
+  C:::::CCCCCCCC::::Ca::::a    a:::::ac:::::::cccccc:::::c      t::::::tttt:::::ti::::::i
+   CC:::::::::::::::Ca:::::aaaa::::::a c:::::::::::::::::c      tt::::::::::::::ti::::::i
+     CCC::::::::::::C a::::::::::aa:::a cc:::::::::::::::c        tt:::::::::::tti::::::i
+        CCCCCCCCCCCCC  aaaaaaaaaa  aaaa   cccccccccccccccc          ttttttttttt  iiiiiiii
+
+
+EOF
+  echo -n "${reset}"
+  echo "Welcome to the Kevin's Cacti script!"
+  echo
+}
+
 case $1 in
 	dev)
 		echo -e "\033[31m Switching to DEV script..."
@@ -22,17 +55,21 @@ case $1 in
 		bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/dev/upgrade-cacti.sh) $1 $2
 	;;
 	--fix-permissions)
+		welcomeMessage
 		counter=$( curl -s http://www.kevinnoall.com/cgi-bin/counter/unicounter.pl?name=update-permissions&write=0 )
-		bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/master/update-permissions.sh) $1
+		bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/master/update-permissions.sh) $1 $2
 	;;
 	--backup-data)
+		welcomeMessage
 		counter=$( curl -s http://www.kevinnoall.com/cgi-bin/counter/unicounter.pl?name=backup-data&write=0 )
-		bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/master/backup-cacti.sh) $1
+		bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/master/backup-cacti.sh) $1 $2
 	;;
 	--restore-data)
-		bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/master/restore-cacti.sh)
+		welcomeMessage
+		bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/master/restore-cacti.sh) $1 $2
 	;;
 	*)
+		welcomeMessage
 		bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/master/upgrade-cacti.sh) $1 $2
 	;;
 esac
