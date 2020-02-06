173333efdcffc1a73b162df8aeddf3e4d48e157b
KnoAll <7632685+KnoAll@users.noreply.github.com>
2020-02-05 20:35:44 -0800

    Update backup-cacti.sh

--- backup-cacti.sh
+++ backup-cacti.sh
@@ -2,20 +2,44 @@
 
 #bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/dev/backup-cacti.sh)
 
+green=$(tput setaf 2)
+red=$(tput setaf 1)
+tan=$(tput setaf 3)
+reset=$(tput sgr0)
+errorcount=0
+branch=master
+
+printinfo() {
+	printf "${tan}::: ${green}%s${reset}\n" "$@"
+}
+
+printwarn() {
+	printf "${tan}*** WARNING: %s${reset}\n" "$@"
+}
+
+printerror() {
+	printf "${red}!!! ERROR: %s${reset}\n" "$@"
+}
+
 case $(whoami) in
         root)
-		echo -e "\033[31m You ran me as root! Do not run me as root!"
-		echo -e -n "\033[0m"
+		printerror "You ran me as root! Do not run me as root!"
 		exit 1
 		;;
         pi)
-		echo -e "\033[31m You ran me as pi user! Do not run me as pi!"
-		echo -e -n "\033[0m"
+		printerror "You ran me as pi user! Do not run me as pi!"
 		exit 1
                 ;;
-        cacti)       
-                echo -e "\033[32m Grabbing Cacti db and data and packaging..."
-                echo -e -n "\033[0m"
+        cacti)
+                ;;
+        *)
+		printerror "Uh-oh. You are not logged in as the cacti user. Exiting..."
+		exit 1
+                ;;
+esac
+
+backupData() {
+                printinfo "Grabbing Cacti db and data and packaging..."
                 cactiver=$( cat /var/www/html/cacti/include/cacti_version )
                 mkdir cacti_$cactiver
                 mysqldump --user=cacti --password=cacti -l --add-drop-table cacti |gzip > ~/cacti_$cactiver/mysql.cacti_$(date +\%Y\%m\%d).sql.gz
@@ -23,12 +47,7 @@ case $(whoami) in
 		echo $cactiver > cacti_$cactiver/.cacti-backup
                 tar -pczf ~/backup_cacti-$cactiver_$(date +\%Y\%m\%d).tar.gz -C ~/ cacti_$cactiver
 		rm -rf cacti_$cactiver
-                ;;
-        *)
-		echo -e "\033[31m Uh-oh. You are not logged in as the cacti user. Exiting..."
-		echo -e -n "\033[0m"
-		exit 1
-                ;;
-esac
+}
 
-exit
+backupData
+exit 0
