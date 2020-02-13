39e8f522963ff971808a68edf1d1f7489832f423
KnoAll <7632685+KnoAll@users.noreply.github.com>
2020-02-12 19:10:23 -0800

    Update update-permissions-smokeping.sh

--- update-permissions-smokeping.sh
+++ update-permissions-smokeping.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
 #bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/dev/update-permissions-smokeping.sh)
+green=$(tput setaf 2)
+red=$(tput setaf 1)
+tan=$(tput setaf 3)
+reset=$(tput sgr0)
+errorcount=0
+branch=master
+
+printinfo() {
+	printf "${tan}::: ${green}%s${reset}\n" "$@"
+}
+printwarn() {
+	printf "${tan}*** WARNING: %s${reset}\n" "$@"
+}
+printerror() {
+	printf "${red}!!! ERROR: %s${reset}\n" "$@"
+}
 
 if which yum >/dev/null; then
 	pkg_mgr=yum
 elif which apt >/dev/null; then
 	pkg_mgr=apt
 else
-		echo -e "\033[31m You seem to be on something other than CentOS or Raspian, cannot proceed..."
-		echo -e -n "\033[0m"
+		printerror "You seem to be on something other than CentOS or Raspian, cannot proceed..."
 		exit 1
 fi
 
-echo -e "\033[32m Fixing file permissions..."
-echo -e -n "\033[0m"
+printinfo "Fixing file permissions..."
 if [[ $pkg_mgr == "yum" ]]; then
 	perm_grp=apache
 else
