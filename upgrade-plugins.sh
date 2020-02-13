a5fcb70aa1e13f0cc2da3e0be691ca5a02b8e136
KnoAll <7632685+KnoAll@users.noreply.github.com>
2020-02-12 19:02:03 -0800

    Update upgrade-plugins.sh

--- upgrade-plugins.sh
+++ upgrade-plugins.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 # bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/dev/upgrade-plugins.sh)
+green=$(tput setaf 2)
+red=$(tput setaf 1)
+tan=$(tput setaf 3)
+reset=$(tput sgr0)
+errorcount=0
+branch=master
+
+printinfo() {
+	printf "${tan}::: ${green}%s${reset}\n" "$@"
+}
+printwarn() {
+	printf "${tan}*** WARNING: %s${reset}\n" "$@"
+}
+printerror() {
+	printf "${red}!!! ERROR: %s${reset}\n" "$@"
+}
 
 case $1 in
 	dev)
@@ -8,6 +24,8 @@ case $1 in
 	;;
 	develop)
 		branch=develop
+	;;
+	--switch-dev)
 	;;
 	*)
 		branch=master
@@ -23,13 +41,12 @@ do
   git status >/dev/null 2>&1
   # check if exit status of above was 0, indicating we're in a git repo
 #  [ $(echo $?) -eq 0 ] && echo "Updating ${dir%*/}..." && git checkout master && git pull
-  [ $(echo $?) -eq 0 ] && echo "Updating ${dir%*/}..." && git pull
+  [ $(printinfo "$?") -eq 0 ] && echo "Updating ${dir%*/}..." && git pull
   cd ..
 done
 
 function update-syslog-config () {
-echo -e "\033[32m Updating syslog plugin config..."
-echo -e -n "\033[0m"
+printinfo "Updating syslog plugin config..."
 cd /var/www/html/
 cp cacti/plugins/syslog/config.php.dist cacti/plugins/syslog/config.php
 sed -i 's/cactiuser/cacti/g' cacti/plugins/syslog/config.php
