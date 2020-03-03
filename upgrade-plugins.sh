464784b29e450bf8c544e3d837e8247ed05c3c56
KnoAll <7632685+KnoAll@users.noreply.github.com>
2020-03-03 13:16:58 -0600

    Update upgrade-plugins.sh

--- upgrade-plugins.sh
+++ upgrade-plugins.sh
@@ -40,7 +40,7 @@ do
   cd ${dir}
   git status >/dev/null 2>&1
   # check if exit status of above was 0, indicating we're in a git repo
-	[ $(echo $?) -eq 0 ] && echo "Updating ${dir%*/}..." && git checkout master && git pull
+	[ $(echo $?) -eq 0 ] && echo "Updating ${dir%*/}..." && git pull
 #[ $(printinfo $?) -eq 0 ] && echo "Updating ${dir%*/}..." && git checkout master && git pull
 #[ $(printinfo "$?") -eq 0 ] && echo "Updating ${dir%*/}..." && git pull
   
