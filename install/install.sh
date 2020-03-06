401dc9901c0d0981ad7e72b1a78efb2486aaa173
KnoAll <7632685+KnoAll@users.noreply.github.com>
2020-03-06 12:43:12 -0800

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -156,7 +156,7 @@ fi
 
 printwarn "Installing prerequisites, this may take a while too..."
 if [[ $os_dist == "raspbian" ]]; then
-	sudo apt -y -qq install autoconf dos2unix unattended-upgrades php libapache2-mod-php php-mbstring php-gmp mariadb-server mariadb-client php-mysqlnd php-curl php-net-socket php-gd php-intl php-pear php-imap php-memcache php-pspell php-recode php-tidy php-xmlrpc php-snmp php-mbstring php-gettext php-gmp php-json php-xml php-common snmp snmpd snmp-mibs-downloader rrdtool php-ldap php-snmp sendmail gcc libssl-dev libmariadbclient-dev libperl-dev libsnmp-dev help2man default-libmysqlclient-dev git
+	sudo apt -y -qq install autoconf dos2unix unattended-upgrades php libapache2-mod-php php-mbstring php-gmp mariadb-server mariadb-client php-mysql php-curl php-net-socket php-gd php-intl php-pear php-imap php-memcache php-pspell php-recode php-tidy php-xmlrpc php-snmp php-mbstring php-gettext php-gmp php-json php-xml php-common snmp snmpd snmp-mibs-downloader rrdtool php-ldap php-snmp sendmail gcc libssl-dev libmariadbclient-dev libperl-dev libsnmp-dev help2man default-libmysqlclient-dev git
 	if [ $? -ne 0 ];then
 		printerror "Something went wrong installing prerequisites, exiting..."
 		exit 1
@@ -167,7 +167,7 @@ if [[ $os_dist == "raspbian" ]]; then
 elif [[ $os_dist == "centos" ]]; then
 	curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
 	sudo sed -i 's/enforcing/permissive/g' /etc/selinux/config
-	sudo yum install -y -q httpd php php-mysql MariaDB-server MariaDB-shared rrdtool net-snmp net-snmp-utils autoconf automake libtool dos2unix help2man openssl-devel MariaDB-devel net-snmp-devel nano wget git php-gd php-mbstring php-snmp php-ldap php-posix
+	sudo yum install -y -q httpd php php-mysqlnd MariaDB-server MariaDB-shared rrdtool net-snmp net-snmp-utils autoconf automake libtool dos2unix help2man openssl-devel MariaDB-devel net-snmp-devel nano wget git php-gd php-mbstring php-snmp php-ldap php-posix
 	if [ $? -ne 0 ];then
 		printerror "Something went wrong installing prerequisites, exiting..."
 		exit 1
