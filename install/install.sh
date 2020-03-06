027630a9bb385dd74bbe0ccbc1a61683f5cbae4d
KnoAll <7632685+KnoAll@users.noreply.github.com>
2020-03-06 13:39:05 -0800

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -181,7 +181,9 @@ case $os_name in
 	CentOS8)
 		curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
 		sudo sed -i 's/enforcing/permissive/g' /etc/selinux/config
+		printinfo "Setting up help2man"
 		sudo dnf --enablerepo=PowerTools install -y help2man
+		printinfo "Setting up packages"
 		sudo yum install -y -q httpd php php-mysqlnd MariaDB-server MariaDB-shared rrdtool net-snmp net-snmp-utils autoconf automake libtool dos2unix openssl-devel MariaDB-devel net-snmp-devel nano wget git php-gd php-mbstring php-snmp php-ldap php-posix php-json
 		if [ $? -ne 0 ];then
 			printerror "Something went wrong installing prerequisites, exiting..."
