24ff8b55e1d3fc6b5aff28c302a13654d77368fc
KnoAll <7632685+KnoAll@users.noreply.github.com>
2020-10-14 15:30:17 -0700

    Update upgrade-cacti.sh

--- upgrade-cacti.sh
+++ upgrade-cacti.sh
@@ -66,9 +66,9 @@ fi
 
 # get latest version of cacti-upgrade script
 if grep -q v1.2.14 cacti-upgrade.sh; then
-	printinfo ""
+	printinfo
 else
-	printinfo "Upgrading cacti.upgrade.sh"
+	printinfo "Upgrading cacti-upgrade.sh"
 	rm cacti-upgrade.sh
 	wget -q https://raw.githubusercontent.com/KnoAll/cacti-template/$branch/cacti-upgrade.sh
 	chmod +x cacti-upgrade.sh
