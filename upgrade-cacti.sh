a20adeadcc8f4fca51b97661e3f2770b4ac5342d
KnoAll <7632685+KnoAll@users.noreply.github.com>
2021-03-16 15:00:33 -0700

    Update upgrade-cacti.sh

--- upgrade-cacti.sh
+++ upgrade-cacti.sh
@@ -360,6 +360,9 @@ function cron () {
 		disable )
 			printwarn "Disabling cronjob"
 			crontab -l | sed '/\/cacti\/poller\.php/s/^/#/' | crontab -
+				if [ $? -ne 0 ];then
+					printerror "Re-Enabling cronjob failed, please check crontab -e"
+				fi
 		;;
 		* ) 
 			printwarn "You have entered an invallid selection!"
