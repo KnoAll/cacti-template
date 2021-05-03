2326e2aef2b341533a9e64b3b673735eb4d49b56
KnoAll <7632685+KnoAll@users.noreply.github.com>
2021-05-03 10:49:27 -0700

    Update upgrade-cacti.sh

--- upgrade-cacti.sh
+++ upgrade-cacti.sh
@@ -401,11 +401,11 @@ else
 	fi
 fi
 printinfo "Restoring data"
-rsync -a -q --ignore-existing cacti_$cactiver/rra/* cacti/rra/
-rsync -a -q --ignore-existing cacti_$cactiver/scripts/* cacti/scripts/
-rsync -a -q --ignore-existing cacti_$cactiver/resource/* cacti/resource/
-rsync -a -q --ignore-existing cacti_$cactiver/plugins/* cacti/plugins/
-rsync -a -q --ignore-existing cacti_$cactiver/include/themes cacti/include/themes/
+rsync -raq --ignore-existing cacti_$cactiver/rra cacti/rra
+rsync -raq --ignore-existing cacti_$cactiver/scripts cacti/scripts
+rsync -raq --ignore-existing cacti_$cactiver/resource cacti/resource
+rsync -raq --ignore-existing cacti_$cactiver/plugins cacti/plugins
+rsync -raq --ignore-existing cacti_1.2.8/include/themes cacti/include/themes
 #update-config
 update-permissions
 printinfo
