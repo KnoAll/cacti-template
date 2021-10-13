3393e2a0a299a6a4bdd4f6395339c431b01041f1
KnoAll <7632685+KnoAll@users.noreply.github.com>
2021-10-13 13:28:00 -0700

    Update backup-cacti.sh

--- backup-cacti.sh
+++ backup-cacti.sh
@@ -40,7 +40,7 @@ exit_on_error() {
     exit_code=$1
     last_command=${@:2}
     if [ $exit_code -ne 0 ]; then
-        >&2 echo "\"${last_command}\" command failed with exit code ${exit_code}."
+        printerror "${last_command} command failed with exit code ${exit_code}."
         exit $exit_code
     fi
 }
