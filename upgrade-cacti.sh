ab4b4f03046704cff3bbecfc1cbf8f0af9e21bf5
KnoAll <7632685+KnoAll@users.noreply.github.com>
2022-04-06 16:59:03 -0700

    Update upgrade-cacti.sh

--- upgrade-cacti.sh
+++ upgrade-cacti.sh
@@ -216,7 +216,6 @@ if version_ge $cactiver $upgrade_version; then
 		#check for PHP version upgrade
 		bash <(curl -s https://raw.githubusercontent.com/KnoAll/cacti-template/$branch/upgrade-php.sh) $param1
 		phpStatus=$?
-		printerror "phpstatus is $phpStatus"
 		case "$phpStatus" in
 		167 )
 			printerror "PHP is not at minimum version, cannot proceed. To automatically upgrade Cacti please upgrade PHP to v7.2 or higher. Exiting."
