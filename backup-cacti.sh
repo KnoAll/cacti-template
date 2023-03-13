56d3b572d1c29a802f6bebe39abcb84756cf58a2
KnoAll <7632685+KnoAll@users.noreply.github.com>
2023-03-13 11:50:09 -0700

    Update backup-cacti.sh

--- backup-cacti.sh
+++ backup-cacti.sh
@@ -66,6 +66,7 @@ backupData() {
                 mkdir cacti_$cactiver
                 mysqldump --user=cacti --password=cacti -l --add-drop-table cacti |gzip > ~/cacti_$cactiver/mysql.cacti_$(date +\%Y\%m\%d).sql.gz
 		if [ $? -ne 0 ];then
+			printerror "trying to backup cactimain"
 			mysqldump --user=cacti --password=cacti -l --add-drop-table cactimain |gzip > ~/cacti_$cactiver/mysql.cacti_$(date +\%Y\%m\%d).sql.gz
 			if [ $? -ne 0 ];then
 				printerror "Error backing up alternate Cacti db cactimain, DATABASE NOT BACKED UP! You should back up manually."
