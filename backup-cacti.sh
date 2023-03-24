f0f1d3d906da309ba9e82ee975d2880bab6ddae3
KnoAll <7632685+KnoAll@users.noreply.github.com>
2023-03-24 11:56:23 -0700

    Update backup-cacti.sh

--- backup-cacti.sh
+++ backup-cacti.sh
@@ -62,14 +62,14 @@ backupData() {
                 printinfo "Grabbing Cacti db..."
                 cactiver=$( cat /var/www/html/cacti/include/cacti_version )
                 mkdir /"$storepath"/cacti_$cactiver
-                mysqldump --user=cacti --password=cacti -l --add-drop-table cacti > ~/cacti_$cactiver/mysql.cacti_$(date +\%Y\%m\%d).sql
+                mysqldump --user=cacti --password=cacti -l --add-drop-table cacti > /"$storepath"/cacti_$cactiver/mysql.cacti_$(date +\%Y\%m\%d).sql
 		if [[ $? -ne 0 ]];then
 			printwarn "Error backing up default Cacti db, trying to backup alternate db cactimain"
-			mysqldump --user=cacti --password=cacti -l --add-drop-table cactimain > ~/cacti_$cactiver/mysql.cacti_$(date +\%Y\%m\%d).sql
+			mysqldump --user=cacti --password=cacti -l --add-drop-table cactimain > /"$storepath"/cacti_$cactiver/mysql.cacti_$(date +\%Y\%m\%d).sql
 			if [[ $? -ne 0 ]];then
 				printerror "Error backing up alternate Cacti db cactimain, DATABASE NOT BACKED UP! You should back up manually."
 			else
-				gzip ~/cacti_$cactiver/mysql.cacti_$(date +\%Y\%m\%d).sql
+				gzip /"$storepath"/cacti_$cactiver/mysql.cacti_$(date +\%Y\%m\%d).sql
 				if [[ $? -ne 0 ]];then
 					printerror "Cacti db NOT BACKED UP!"
 				else
@@ -77,7 +77,7 @@ backupData() {
 				fi				
 			fi
 		else
-		gzip ~/cacti_$cactiver/mysql.cacti_$(date +\%Y\%m\%d).sql
+		gzip /"$storepath"/cacti_$cactiver/mysql.cacti_$(date +\%Y\%m\%d).sql
 			if [[ $? -ne 0 ]];then
 				printerror "Cacti database NOT BACKED UP! You should backup the db manually."
 			else
@@ -85,19 +85,19 @@ backupData() {
 			fi
 		fi
 		printinfo "Grabbing configs, RRA files, resources, scripts, etc...."
-                cp -R /var/www/html/cacti/rra ~/cacti_$cactiver/rra
-		rsync -raq /var/www/html/cacti/resource ~/cacti_$cactiver/
-		rsync -raq /var/www/html/cacti/scripts ~/cacti_$cactiver/
-		rsync -raq /var/www/html/cacti/include/themes ~/cacti_$cactiver/
-		cp /var/www/html/cacti/include/config.php ~/cacti_$cactiver
-		cp /usr/local/spine/etc/spine.conf ~/cacti_$cactiver
-		echo $cactiver > cacti_$cactiver/.cacti-backup
+                cp -R /var/www/html/cacti/rra /"$storepath"/cacti_$cactiver/rra
+		rsync -raq /var/www/html/cacti/resource /"$storepath"/cacti_$cactiver/
+		rsync -raq /var/www/html/cacti/scripts /"$storepath"/cacti_$cactiver/
+		rsync -raq /var/www/html/cacti/include/themes /"$storepath"/cacti_$cactiver/
+		cp /var/www/html/cacti/include/config.php /"$storepath"/cacti_$cactiver
+		cp /usr/local/spine/etc/spine.conf /"$storepath"/cacti_$cactiver
+		echo $cactiver > /"$storepath"/cacti_$cactiver/.cacti-backup
 		printinfo
 		printinfo "Compressing files..."
-                tar -pczf ~/backup_cacti-$cactiver_$(date +\%Y\%m\%d).tar.gz -C ~/ cacti_$cactiver
+                tar -pczf /"$storepath"/backup_cacti-$cactiver_$(date +\%Y\%m\%d).tar.gz -C ~/ cacti_$cactiver
 		printinfo "Removing temp files..."
-		rm -rf cacti_$cactiver
-		printinfo "Cacti v$cactiver backed up into ~/backup_cacti-$cactiver_$(date +\%Y\%m\%d).tar.gz"
+		rm -rf /"$storepath"/cacti_$cactiver
+		printinfo "Cacti v$cactiver backed up into /"$storepath"/backup_cacti-$cactiver_$(date +\%Y\%m\%d).tar.gz"
 }
 
 #ingest options
