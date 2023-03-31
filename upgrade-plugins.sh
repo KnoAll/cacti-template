88fe7d4b57bf04ae36a152097fb09825f9746d6e
KnoAll <7632685+KnoAll@users.noreply.github.com>
2023-03-31 11:08:51 -0700

    Update upgrade-plugins.sh

--- upgrade-plugins.sh
+++ upgrade-plugins.sh
@@ -37,6 +37,6 @@ for dir in ./*/; do
 	cd ${dir}
 	git status >/dev/null 2>&1
 	# check if exit status of above was 0, indicating we're in a git repo
-	[ $(echo $?) -eq 0 ] && echo "Updating ${dir%*/}..." && git pull
+	[ $(echo $?) -eq 0 ] && printinfo "Updating ${dir%*/}..." && git pull
 	cd ..
 done
