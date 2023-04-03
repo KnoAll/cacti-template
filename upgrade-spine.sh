54df272433257ec4758c9ac4ed7a9fd5e7bc5263
KnoAll <7632685+KnoAll@users.noreply.github.com>
2023-04-03 14:24:26 -0700

    Update upgrade-spine.sh

--- upgrade-spine.sh
+++ upgrade-spine.sh
@@ -9,13 +9,17 @@ errorcount=0
 branch=master
 
 printinfo() {
-	printf "${tan}::: ${green}%s${reset}\n" "$@"
+	if [ -z "$1" ]; then
+		printf "${tan}::: ${green}%s${reset}\n" "$@"
+	else
+		printf "${tan}::: ${green}%s${reset}\n" "$(date +%a_%R): $@"
+	fi	
 }
 printwarn() {
-	printf "${tan}*** WARNING: %s${reset}\n" "$@"
+	printf "${tan}*** WARNING: %s${reset}\n" "$(date +%a_%R): $@"
 }
 printerror() {
-	printf "${red}!!! ERROR: %s${reset}\n" "$@"
+	printf "${red}!!! ERROR: %s${reset}\n" "$(date +%a_%R): $@"
 }
 
 #ingest options
